2d15a8791010a84080bdaf19436acd63f65aa37d
PaavoAhola <102104755+PaavoAhola@users.noreply.github.com>
2022-05-15 23:41:05 +0300

    Update user.py

--- src/entities/user.py
+++ src/entities/user.py
@@ -1,7 +1,27 @@
 class User:
+    """Sovelluksen käyttäjää kuvaava luokka.
+    
+    Attributes:
+        __name: Käyttäjän nimi.
+        __password: Käyttäjän salasana.
+    """
+    
     def __init__(self, name, password):
+        """Konstruktori, joka luo käyttäjäolion.
+        
+        Args:
+            name: Käyttäjän nimi.
+            password: Käyttäjän salasana.
+        """
+        
         self.__name = name
         self.__password = password
 
     def return_name(self):
+        """Palauttaa käyttäjän nimen.
+        
+        Returns:
+            Käyttäjän nimi.
+        """
+        
         return self.__name
